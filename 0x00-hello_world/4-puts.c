e45b71ef725e04785937ddee24e18186fffac314
Olawale Samuel <olawalesamuel147@gmail.com>
2023-06-15 08:59:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-
-/**
- *
- * main: print a string of char using puts funtion
- *
- * return- 0 if successful
- *
- */
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-}	
+#include <stdio.h> 
+  
+ /** 
+  * main - Prints a string of char. 
+  * 
+  * Return: 0 if successful 
+  */ 
+  
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+  
+         return (0); 
+ }

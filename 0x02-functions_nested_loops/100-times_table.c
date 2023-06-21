eca310d8f6d2c3014c9ca40d926bfeef5840788d
Olawale Samuel <olawalesamuel147@gmail.com>
2023-06-21 14:36:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,90 @@
 #include "main.h"
 
 /**
- * print_times_table - Print the 'n' times table, starting with 0.
- * @n: number of times tables to print
- * Return: void
+
+ * print_times_table - prints the n times table, starting with 0
+
+ * @n: number of the times table
+
  */
 
 void print_times_table(int n)
+
 {
-	int a = 0, b, c;
 
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			c = a * b;
-			if (c > 99)
-			{
-				_putchar(c / 100 + '0');
-				_putchar((c / 10 % 10) + '0');
-				_putchar(c % 10 + '0');
-			}
-			else if (c > 9)
-			{
-				_putchar(' ');
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
-			}
-			else if (b != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			}
-			else
-				_putchar(c + '0');
+        int i, j, k;
 
-			if (b != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		a++;
-	}
+
+        if (n >= 0 && n <= 15)
+
+        {
+
+                for (i = 0; i <= n; i++)
+
+                {
+
+                        for (j = 0; j <= n; j++)
+
+                        {
+
+                                k = j * i;
+
+                                if (j == 0)
+
+                                {
+
+                                        _putchar(k + '0');
+
+                                } else if (k < 10 && j != 0)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar(k + '0');
+
+                                } else if (k >= 10 && k < 100)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar((k / 10) + '0');
+
+                                        _putchar((k % 10) + '0');
+
+                                } else if (k >= 100)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar((k / 100) + '0');
+
+                                        _putchar(((k / 10) % 10) + '0');
+
+                                        _putchar((k % 10) + '0');
+
+                                }
+
+                        }
+
+                        _putchar('\n');
+
+                }
+
+        }
+
 }
